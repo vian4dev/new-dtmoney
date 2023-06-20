f2c2a28479973969e98c1476181ad1308338c55f
Gabriel Viana <viannadoze@gmail.com>
2023-06-20 11:31:20 -0300

    Transaction table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return(
         <div>
             <Header/>
             <Summary/>
+
+            <TransactionsContainer>
+            <TransactionsTable>
+                <tbody>
+                    <tr>
+                        <td width="50%">Desenvolvimento de site</td>
+                        <td>
+                            <PriceHighLight variant="income">
+                                R$ 12.000,00
+                            </PriceHighLight>
+                        </td>
+                        <td>Venda</td>
+                        <td>19/06/2023</td>
+                    </tr>
+                    <tr>
+                        <td width="50%">Hamburger</td>
+                        <td> 
+                            <PriceHighLight variant="outcome">
+                                - R$ 59,00
+                            </PriceHighLight>
+                        </td>
+                        <td>Alimentação</td>
+                        <td>20/06/2023</td>
+                    </tr>
+                </tbody>
+            </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
