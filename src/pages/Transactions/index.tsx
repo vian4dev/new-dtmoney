9152d35d88ec4d9e517d7a8baea3757629db3ef6
Gabriel Viana <viannadoze@gmail.com>
2023-06-21 10:12:00 -0300

    Component: SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,38 +1,41 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
-    return(
+    return (
         <div>
-            <Header/>
-            <Summary/>
+            <Header />
+            <Summary />
 
             <TransactionsContainer>
-            <TransactionsTable>
-                <tbody>
-                    <tr>
-                        <td width="50%">Desenvolvimento de site</td>
-                        <td>
-                            <PriceHighLight variant="income">
-                                R$ 12.000,00
-                            </PriceHighLight>
-                        </td>
-                        <td>Venda</td>
-                        <td>19/06/2023</td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Hamburger</td>
-                        <td> 
-                            <PriceHighLight variant="outcome">
-                                - R$ 59,00
-                            </PriceHighLight>
-                        </td>
-                        <td>Alimentação</td>
-                        <td>20/06/2023</td>
-                    </tr>
-                </tbody>
-            </TransactionsTable>
+                <SearchForm />
+                
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighLight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>19/06/2023</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hamburger</td>
+                            <td>
+                                <PriceHighLight variant="outcome">
+                                    - R$ 59,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>20/06/2023</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
             </TransactionsContainer>
         </div>
     )
