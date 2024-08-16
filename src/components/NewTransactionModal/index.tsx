dbdc0acc49ce2f1c63c2aec036789c10ef360efc
Gabriel Viana <viannadoze@gmail.com>
2024-08-16 14:36:29 -0300

    Botões de entrada e saída

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { CloseButton, Content, Overlay } from "./styles";
-import { X } from "phosphor-react";
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
 export function NewTransactionModal() {
     return (
@@ -10,19 +10,31 @@ export function NewTransactionModal() {
                     <Dialog.Title>Nova transação</Dialog.Title>
 
                     <CloseButton>
-                        <X />
+                        <X size={24} />
                     </CloseButton>
 
                     <form>
-                        <input type="text" placeholder="Descrição" required/>
-                        <input type="number" placeholder="Categoria" required/>
-                        <input type="text" placeholder="Preço" required/>
+                        <input type="text" placeholder="Descrição" required />
+                        <input type="number" placeholder="Categoria" required />
+                        <input type="text" placeholder="Preço" required />
+
+                        <TransactionType>
+                            <TransactionTypeButton variant={"income"}>
+                                <ArrowCircleUp size={24}/>
+                                Entrada
+                            </TransactionTypeButton>
+
+                            <TransactionTypeButton variant={"outcome"}>
+                                <ArrowCircleDown size={24}/>
+                                Saída
+                            </TransactionTypeButton>
+                        </TransactionType>
 
                         <button type="submit">
                             Cadastrar
                         </button>
                     </form>
-                    
+
                 </Content>
             </Overlay>
         </Dialog.Portal>
