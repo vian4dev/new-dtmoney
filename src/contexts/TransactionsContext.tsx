8d28c24a9e5de85c5f90f4f9a2881fd3925f69f7
Gabriel Viana <viannadoze@gmail.com>
2024-12-05 14:53:26 -0300

    Criando contexto de transacoes

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,44 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
+interface TransactionContextType {
+  transactions: Transaction[];
+}
+
+interface TransactionsProviderProps {
+  children: ReactNode
+}
+
+export const TransactionsContext = createContext({} as TransactionContextType);
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+    async function loadTransaction() {
+        const response = await fetch('http://localhost:3333/transactions')
+        const data = await response.json();
+        console.log(data)
+
+        setTransactions(data);
+    }
+
+    useEffect(() => {
+        loadTransaction();
+    }, [])
+  
+  return (
+    <TransactionsContext.Provider value={{ transactions }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
+
+
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,32 +1,14 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
-interface Transaction {
-    id: number;
-    description: string;
-    type: 'income' | 'outcome';
-    price: number
-    category: string;
-    createdAt: string;
-}
+
 
 export function Transactions() {
-    const [transactions, setTransactions] = useState<Transaction[]>([])
-
-    async function loadTransaction() {
-        const response = await fetch('http://localhost:3333/transactions')
-        const data = await response.json();
-        console.log(data)
-
-        setTransactions(data);
-    }
-
-    useEffect(() => {
-        loadTransaction();
-    }, [])
+    const { transactions} = useContext(TransactionsContext);
 
     return (
         <div>
